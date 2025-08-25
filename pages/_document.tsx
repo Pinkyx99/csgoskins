7bfd424e99e8897b682fb2b00ca5d7cedfb16198
Pinkyx99 <alijaramill@gmail.com>
2025-08-25 17:10:21 +0200

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Search Console meta tag */}
+        <meta name="google-site-verification" content="KpBs9kYX31-VZ28vkXDIrRV-WviIdedv6vRkLgOhsRM" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
